29dfb8e0eed271d71db1c863e5e3900df015f322
Đạt Phan <t Phan@DESKTOP-R9F1HPG>
2017-09-29 21:31:07 +0700

    Add project files.

---
+++ PhanTanDat-b1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+
+namespace PhanTanDat_b1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lấy tất cả địa chỉ IPv4 và IPv6 của máy
+            //string HostName = Dns.GetHostName();
+            //Console.WriteLine("Computer name is :" + HostName);
+            //IPAddress[] ipaddress = Dns.GetHostAddresses(HostName);
+            //Console.WriteLine("IP Address are :");
+            //foreach (IPAddress ip in ipaddress)
+            //{
+            //    Console.WriteLine(ip.ToString());
+            //}
+            //Lấy địa chỉ IPv4 của máy
+            string HostName = Dns.GetHostName();
+            Console.WriteLine("Computer name =" + HostName);
+            IPAddress[] ipaddress = Dns.GetHostAddresses(HostName);
+            Console.WriteLine("IPv4 of your computer are ");
+            foreach (IPAddress ipv4 in ipaddress.Where(ip => ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork))
+            {
+                Console.WriteLine(ipv4.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PhanTanDat-b2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PhanTanDat_b2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.Write("Input hostname or ip address : ");
+                string host = Console.ReadLine();
+
+                IPHostEntry iphost = Dns.GetHostEntry(host);
+                Console.WriteLine("Hostname: {0}", iphost.HostName);
+
+
+            foreach (string s in iphost.Aliases)
+            {
+                Console.WriteLine("\t+ Alias: {0} \n", s);
+            }
+
+            foreach (IPAddress i in iphost.AddressList)
+            {
+                Console.WriteLine("\t+ IP: {0}", i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
